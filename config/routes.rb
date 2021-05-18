040d29936b94e0b5e9d17a301c0336789055fbca
OguraYuto <ogryut0121@gmail.com>
2021-05-18 09:11:21 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ root 'homes#top'
+ get 'home/about' => 'homes#about'
+ resources :items
+ resources :customers
+ resources :cart_items
+ resources :orders
+ resources :deliveries
+
+ namespace :admin do
+    root 'homes#top'
+    resources :products
+    resources :customers
+    resources :genres
+    resources :orders do
+        patch :order_details
+    end
+ end
+end
+
+
