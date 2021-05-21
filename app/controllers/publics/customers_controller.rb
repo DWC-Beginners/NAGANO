78ef0410448a6339c28ecf8f1fc9dca7aa3a1515
Fukui-S <tibita13579@yahoo.co.jp>
2021-05-21 12:08:28 +0000

    [Add]customer/view3object customer/controller

--- app/controllers/publics/customers_controller.rb
+++ app/controllers/publics/customers_controller.rb
@@ -1,2 +1,39 @@
 class Publics::CustomersController < Publics::ApplicationController
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  # 更新成功失敗時にフラッシュメッセージ必要か？
+  def update
+    @customer = Customer.find(params[:id])
+    if customer.update(customer_params)
+      redirect_to customer_path(current_customer.id)
+    else
+      render :edit
+    end
+  end
+
+  def unsubscribe
+    @customer = Customer.find(current_customer.id)
+    if @customer.update(is_valid: false)
+      sign_out current_customer
+      redirect_to root_path
+    end
+  end
+  
+  def withdraw
+    
+  end
+
+
+  private
+  def customer_params
+  params.require(:customer).permit(:first_name, :last_name, :first_name_kana,
+    :last_name_kana, :postal_code, :address, :email, :phone_number, :is_deleted, :encrypted_password)
+  end
 end
