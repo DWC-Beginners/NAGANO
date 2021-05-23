d568500ea48026fa7ce6ea6e19adf96037df85e8
shuto3726 <sss3726mmm@gmail.com>
2021-05-23 01:21:02 +0000

    pproduct

--- app/controllers/publics/items_controller.rb
+++ app/controllers/publics/items_controller.rb
@@ -1,2 +1,15 @@
 class Publics::ItemsController < Publics::ApplicationController
+  
+  def index
+    @item = Item.all
+  end
+  
+  def show
+    @item = Item.find(params[:id])
+  end
+  
+  private
+  def product_params
+   params.require(:product).permit(:image, :name, :description)
+ end
 end
