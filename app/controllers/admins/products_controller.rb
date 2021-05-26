fd0c3719097d699d6788462aab9a2444e71c2d02
shuto3726 <sss3726mmm@gmail.com>
2021-05-26 01:54:18 +0000

    develop

--- app/controllers/admins/products_controller.rb
+++ app/controllers/admins/products_controller.rb
@@ -1,7 +1,8 @@
 class Admins::ProductsController < ApplicationController
   
   def index
-    @products = Product.page(params[:page]).reverse_order
+    @product = Product.page(params[:page]).reverse_order
+    @products = Product.all
   end
   
   def show
@@ -13,9 +14,9 @@ class Admins::ProductsController < ApplicationController
   end
   
   def create
-    @product =  Product.new(product_params)
+    @product = Product.new(product_params)
     @product.save
-    redirect_to admin_product_path(product_id)
+    redirect_to admins_product_path(@product)
   end
   
   def edit
@@ -25,15 +26,11 @@ class Admins::ProductsController < ApplicationController
   def update
     product = Product.find(params[:id])
     product.update(product_params)
-    redirect_to admin_product_path(product_id)
+    redirect_to admins_product_path(product_id)
   end
   
   private
   def product_params
-    params.require(:product).permit(:image, :name, :description, :price, :genre_id)
+    params.require(:product).permit(:image, :name, :description, :price, :genre_id, :sale_status)
   end
-<<<<<<< HEAD
 end
-=======
-end
->>>>>>> origin/develop
